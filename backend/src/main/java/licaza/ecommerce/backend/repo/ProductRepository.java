a8190b440b6839c8333e341aaae064a902a6c2fd
Lando <landoicco@gmail.com>
2026-07-08 20:29:06 -0600

    Backend cleaned  - mvn wrapper removed, is not used due Docker  - Extra repository methods removed, not used

--- backend/src/main/java/licaza/ecommerce/backend/repo/ProductRepository.java
+++ backend/src/main/java/licaza/ecommerce/backend/repo/ProductRepository.java
@@ -1,14 +1,8 @@
 package licaza.ecommerce.backend.repo;
 
-import java.util.Optional;
 import licaza.ecommerce.backend.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-  Optional<Product> findByName(String name);
-
-  Optional<Product> findBySku(String sku);
-}
+public interface ProductRepository extends JpaRepository<Product, Long> {}
