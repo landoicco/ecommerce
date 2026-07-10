db82eab3919a92bb948a8529956b47c20eae3e42
Lando <landoicco@gmail.com>
2026-07-10 11:45:23 -0600

    Flyway added and working

--- backend/src/main/java/licaza/ecommerce/backend/domain/Product.java
+++ backend/src/main/java/licaza/ecommerce/backend/domain/Product.java
@@ -3,6 +3,7 @@ package licaza.ecommerce.backend.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
@@ -17,7 +18,9 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Product {
 
-  @Id @GeneratedValue private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
   private String name;
 
