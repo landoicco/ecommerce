08dd70179752876a2a79ebbf0f46d026836bf853
Lando <landoicco@gmail.com>
2026-07-12 09:46:01 -0600

    CORS config improved  - frontend url added as env variable

--- backend/src/main/java/licaza/ecommerce/backend/controller/ProductController.java
+++ backend/src/main/java/licaza/ecommerce/backend/controller/ProductController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/products")
-@CrossOrigin(originPatterns = "http://localhost:[*]")
 public class ProductController {
 
   private final ProductService productService;
